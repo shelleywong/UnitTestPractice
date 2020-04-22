b495afc82ee6962a4335c8723d25e07b8fd348b4
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-22 13:44:35 -0700

    adds sorting test for values that start in backwards order

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -85,3 +85,15 @@ TEST(PracticeTest, sort_second_third)
     ASSERT_GE(first,second);
 		ASSERT_GE(second,third);
 }
+
+// check if values originally in opposite order get sorted correctly
+TEST(PracticeTest, sort_backwards)
+{
+    Practice obj;
+		int first = 1;
+		int second = 2;
+		int third = 3;
+    obj.sortDescending(first, second, third);
+    ASSERT_GE(first,second);
+		ASSERT_GE(second,third);
+}
