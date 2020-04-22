9b4b33f845b54b819f1b807cd56ab0e8a7742f0c
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-22 13:50:23 -0700

    check if repeat values get sorted correctly

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -86,14 +86,26 @@ TEST(PracticeTest, sort_second_third)
 		ASSERT_GE(second,third);
 }
 
-// check if values originally in opposite order get sorted correctly
-TEST(PracticeTest, sort_backwards)
+// check if sorted correctly with repeat values
+TEST(PracticeTest, sort_with_equal_values)
 {
     Practice obj;
 		int first = 1;
-		int second = 2;
+		int second = 1;
 		int third = 3;
     obj.sortDescending(first, second, third);
     ASSERT_GE(first,second);
 		ASSERT_GE(second,third);
 }
+
+// check if values originally in opposite order get sorted correctly
+// TEST(PracticeTest, sort_backwards)
+// {
+//     Practice obj;
+// 		int first = 1;
+// 		int second = 2;
+// 		int third = 3;
+//     obj.sortDescending(first, second, third);
+//     ASSERT_GE(first,second);
+// 		ASSERT_GE(second,third);
+// }
